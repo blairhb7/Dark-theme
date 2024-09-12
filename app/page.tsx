532767818453ca479c23fc8738e0cfdc882b3da2
Blair <Blairhb7@yahoo.com>
2024-09-12 15:58:13 -0700

    spacing updated

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Faq from "./Components/faq";
 
 export default function Home() {
   return (
-    <main className=" mx-auto bg-black">
+    <main className=" mx-auto block bg-black">
       <LandingPage/>
       <Faq/>
       <Footer/>
